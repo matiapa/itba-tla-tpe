9d507bdff1cef1690ab3eca2f6730d0a8e03a44c
Juan Ignacio Sackmann Sala <jsackmann@itba.edu.ar>
2021-06-26 14:20:54 -0300

    fixed frees in var checker

--- src/var_finder.c
+++ src/var_finder.c
@@ -20,11 +20,11 @@ typedef struct var_node {
 
 
 var_node * create_var_node(int type, char * name);
-var_node * add_to_list(var_node * list,var_node * element);
-void check_and_set_variables_internal(node_list * tree,var_node * var_list);
-void free_list(var_node * list);
+void add_to_list(var_node ** list,var_node * element);
+void check_and_set_variables_internal(node_list * tree,var_node ** var_list);
+var_node * free_list(var_node * list);
 int check_if_exists(var_node * list,char * name);
-var_node * check_and_set_variables_rec(node_t * node,var_node * var_list);
+void check_and_set_variables_rec(node_t * node,var_node ** var_list);
 void check_var_types_in_value(variable_node* variable_node_var,var_node * var_list );
 int check_var_type_in_expression(int type,expression_node * expr,var_node * var_list);
 int check_var_type_in_expression_rec(int type,node_t * node,var_node * var_list);
@@ -34,14 +34,18 @@ int check_and_set_variables(node_list * tree){
     return error;
 }
 
-void check_and_set_variables_internal(node_list * tree,var_node * var_list){
+void check_and_set_variables_internal(node_list * tree,var_node ** var_list){
     node_list * aux = tree;
     while (aux != NULL) {
         instruction_node * nodo = (instruction_node *)aux->node;
-        var_list=check_and_set_variables_rec((node_t *)nodo->instruction,var_list);
+        if(var_list!=NULL)
+            check_and_set_variables_rec((node_t *)nodo->instruction,var_list);
         aux = aux->next;
     }
-    free_list(var_list);  //todo mejorar free
+    if (var_list!=NULL)
+    {
+       *var_list=free_list(*var_list);  //todo mejorar free
+    }
     
 }
 
@@ -49,42 +53,42 @@ void check_and_set_variables_internal(node_list * tree,var_node * var_list){
 
 
 
-var_node * check_and_set_variables_rec(node_t * node,var_node * var_list){
+void check_and_set_variables_rec(node_t * node,var_node ** var_list){
     switch(node->type) {
             case VARIABLE_NODE:
                 ;
                 variable_node* variable_node_var=(variable_node *)node;
                 if (variable_node_var->declared==TRUE && variable_node_var->value!=NULL){ //caso donde se define la var y asigna
-                    if (check_if_exists(var_list,variable_node_var->name)!=-1)
+                    if (check_if_exists(*var_list,variable_node_var->name)!=-1)
                     {
                         printf("Var %s already declared \n",variable_node_var->name);
                         error=-1;
                     }
-                    var_list=add_to_list(var_list,create_var_node(variable_node_var->var_type,variable_node_var->name));
+                    add_to_list(var_list,create_var_node(variable_node_var->var_type,variable_node_var->name));
                     
-                    check_var_types_in_value(variable_node_var,var_list);
+                    check_var_types_in_value(variable_node_var,*var_list);
                 }
                 if (variable_node_var->declared==TRUE && variable_node_var->value==NULL){ //caso donde se define la var y no se asigna
-                    if (check_if_exists(var_list,variable_node_var->name)!=-1)
+                    if (check_if_exists(*var_list,variable_node_var->name)!=-1)
                     {
                         printf("Var %s already declared \n",variable_node_var->name);
                         error=-1;
                     }
-                    var_list=add_to_list(var_list,create_var_node(variable_node_var->var_type,variable_node_var->name));
+                    add_to_list(var_list,create_var_node(variable_node_var->var_type,variable_node_var->name));
                 }
                 if (variable_node_var->declared==FALSE && variable_node_var->value!=NULL){ //caso donde no se define la var pero se asigna
-                    int type =check_if_exists(var_list,variable_node_var->name);
+                    int type =check_if_exists(*var_list,variable_node_var->name);
                         if (type==-1)
                         {
                             printf("Var %s not declared yet \n",variable_node_var->name);
                             error=-1;
                         }
                         variable_node_var->var_type=type;
-                        check_var_types_in_value(variable_node_var,var_list);
+                        check_var_types_in_value(variable_node_var,*var_list);
                 }
                 if (variable_node_var->declared==FALSE && variable_node_var->value==NULL){ //caso donde solo se usa la var
                     
-                    int type =check_if_exists(var_list,variable_node_var->name);
+                    int type =check_if_exists(*var_list,variable_node_var->name);
                     if (type==-1)
                     {
                         printf("Var %s not declared yet \n",variable_node_var->name);
@@ -103,7 +107,7 @@ var_node * check_and_set_variables_rec(node_t * node,var_node * var_list){
                         break;          
             
                     case EXPRESSION_NODE:
-                        if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)print_node_var->content,var_list)){
+                        if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)print_node_var->content,*var_list)){
                             printf("Var in write is type text in expression\n");
                             error=-1;
                         }
@@ -123,27 +127,27 @@ var_node * check_and_set_variables_rec(node_t * node,var_node * var_list){
             case IF_NODE:
                 ;
                 if_node* if_node_var=(if_node *)node;
-                if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)if_node_var->condition,var_list)){
+                if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)if_node_var->condition,*var_list)){
                     printf("Var in write is type text in expression\n");
                     error=-1;
                 }
-                var_list->references++;
+                (*var_list)->references++;
                 check_and_set_variables_internal( (node_list *)((block_node *)if_node_var->then)->node_list,var_list);
-                free_list(var_list);
-                var_list->references++;
+                *var_list=free_list(*var_list);
+                (*var_list)->references++;
                 check_and_set_variables_internal((node_list *)((block_node *)if_node_var->otherwise)->node_list,var_list);
-                free_list(var_list);
+                *var_list=free_list(*var_list);
                 break;
             case WHILE_NODE:
                 ;
                 while_node* while_node_var=(while_node *)node;
-                if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)while_node_var->condition,var_list)){
+                if(!check_var_type_in_expression(NUMBER_TYPE,(expression_node *)while_node_var->condition,*var_list)){
                     printf("Var in write is type text in expression\n");
                     error=-1;
                 }
-                var_list->references++;
+                (*var_list)->references++;
                 check_and_set_variables_internal( (node_list *)((block_node *)while_node_var->then)->node_list,var_list);
-                free_list(var_list);
+                 *var_list=free_list(*var_list);
                 break;
             default:
                 #ifdef YYDEBUG
@@ -151,8 +155,6 @@ var_node * check_and_set_variables_rec(node_t * node,var_node * var_list){
                 #endif
                 break;
         }
-        
-        return var_list;
 }
 
 void check_var_types_in_value(variable_node* variable_node_var,var_node * var_list ){
@@ -245,20 +247,18 @@ var_node * create_var_node(int type, char * name){
 
 
 
-var_node * add_to_list(var_node * list,var_node * element){
-    if (list==NULL)
-    {
-        return element;
+void add_to_list(var_node ** list,var_node * element){
+    if (list!=NULL){
+        (*list)->references++;
+        element->next=*list;
+        *list=element;
     }
-    list->references++;
-    element->next=list;
-    return element;
 }
 
-void free_list(var_node * list){
+var_node * free_list(var_node * list){
     if (list == NULL)
     {
-        return;
+        return NULL;
     }
     
     var_node * current=list;
@@ -272,6 +272,7 @@ void free_list(var_node * list){
     {
         current->references--;
     }
+    return current;
 }
 
 int check_if_exists(var_node * list,char * name){
